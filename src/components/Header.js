dac78f04acaa88568562467b137047f8aa499b9d
Fanny Reslow <fr222er@student.lnu.se>
2021-03-07 12:59:27 +0100

    fixing menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,12 +15,9 @@ export default class Header extends React.Component {
                      </div>
                   
                        {_.get(this.props, 'pageContext.site.siteMetadata.header.has_nav', null) && (<React.Fragment>
-                         <button id="menu-open" className="menu-toggle"><span className="screen-reader-text"></span><span className="icon-menu"
-                          aria-hidden="true" /></button>
+                        
                    <nav id="main-navigation" className="site-nav" aria-label="Main Navigation">
                          <div className="site-nav-inside">
-                          <button id="menu-close" className="menu-toggle"><span className="screen-reader-text">Menu</span><span className="icon-close"
-                          aria-hidden="true" /></button>
                           <ul className="menu">
                                 {_.map(_.get(this.props, 'pageContext.site.siteMetadata.header.nav_links', null), (action, action_idx) => {
                                 let pageUrl = _.trim(_.get(this.props, 'pageContext.url', null), '/');
